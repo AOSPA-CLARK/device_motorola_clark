18db3999bb306225fb218cbf8fc495995ad94d2e
Eren Atas <eren.atas@stu.bahcesehir.edu.tr>
2016-10-11 12:44:06 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -84,6 +84,11 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libqdutils \
     libqdMetaData
+    
+# Firmware extraction script
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/releasetools/extract_firmware.sh:install/bin/extract_firmware.sh
+    
 
 # GPS
 PRODUCT_PACKAGES += \
